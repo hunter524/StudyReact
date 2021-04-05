1d4f6dca786575892dff4ab4c5667c80f827d508
hunter <guyuesh2@gmail.com>
2021-04-05 15:12:44 +0800

    js 20 years back up

---
+++ js20years/function.js
@@ -0,0 +1,23 @@
+function countedHello() {
+    console.log("Hello , World!");
+    countedHello.callCount++; // 增加该函数的 callCount 属性
+}
+countedHello.callCount = 0; // 将计数器与函数相关联
+for (var i = 0; i < 5; i++) countedHello();
+console.log(countedHello.callCount); // 显示 5
+var args = [1,2,3,4];
+function argsNumber(){
+    console.log(arguments.length)
+}
+
+argsNumber(...args)// arguments length 5
+argsNumber(args)// arguments length 1
+
+"String".toLocaleLowerCase()
+var upperCaseA = String("aaa").toUpperCase();
+console.log(upperCaseA)
+
+console.log(Date(Date.now()))
+
+// M
+// Math console.log(Math)
