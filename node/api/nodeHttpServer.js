4fdb6b53e0dac4ffe364aa87a298f006805f06e1
hunter <guyuesh2@gmail.com>
2021-08-18 20:36:37 +0800

    添加自动发送邮件相关的代码

--- node/api/nodeHttpServer.js
+++ node/api/nodeHttpServer.js
@@ -1,5 +1,7 @@
 const http = require('http')
-
+// __dirname/__filename 为 nodejs 内置的环境变量
+// 分别表示当前执行 js 脚本所在的目录,以及 js 文件的路径
+console.log("__dirname",__dirname,"__filename",__filename)
 const hostname = '127.0.0.1'
 const port = 3000
 
