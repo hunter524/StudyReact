eee280c7f7b71a87b131cd0118ac19bafb74648e
hunter <guyuesh2@gmail.com>
2021-09-09 10:32:54 +0800

    add nounuo 发票

--- src/nodejs/fpdownload.js
+++ src/nodejs/fpdownload.js
@@ -1,14 +1,15 @@
 const fs         = require('fs');
 const fpdownload = require('download');
-// let data ={}
-let data ={}
+let data = {}
 async function download () {
     for (const value of data.data.list) {
         let fpdate = value.add_time.substr(0,10);
         if (!fs.existsSync(fpdate)){
             fs.mkdirSync(fpdate);
         }
-        fs.writeFileSync(`${fpdate}/${value.c_buyername}_${value.c_kptype}_${value.fphm}.pdf`,await fpdownload(value.pdf_url));
+        if("普通发票(电子)" ==value.c_invoice_line.trimStart().trimEnd()){
+            fs.writeFileSync(`${fpdate}/${value.c_buyername}_${value.c_kptype}_${value.fphm}.pdf`,await fpdownload(value.pdf_url));
+        }
     }
 }
 download().then(r => {})
